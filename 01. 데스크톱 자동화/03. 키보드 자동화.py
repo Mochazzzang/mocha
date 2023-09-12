3e3ac2a1aadd5eddd2f68920c9894a049c70e6e0
Mochazzzang <kominhyuk0923@gmail.com>
2023-09-13 08:15:14 +0900

    230913_mocha

--- 01. 데스크톱 자동화/03. 키보드 자동화.py
+++ 01. 데스크톱 자동화/03. 키보드 자동화.py
@@ -1,8 +1,18 @@
 import pyautogui
+import pyperclip
 
 # 1. 키보드 입력 (문자)
 # pyautogui.write('Mocha',interval=0.1)
 
 # 2. 키보드 입력 (키)
 # pyautogui.press('enter')
-pyautogui.press('up')
+# pyautogui.press('up')
+
+# 3. 키보드 입력 (여러개 동시에)
+# pyautogui.hotkey('ctrl', 'c') # mac = 'command', 'c'
+
+# 4. 한글 입력 방법
+pyperclip.copy('한글 입력')
+pyautogui.hotkey('ctrl', 'v')
+
+
---
+++ 01. 데스크톱 자동화/04. 메세지 박스.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+pyautogui.alert('start?')
+pyautogui.moveTo(500, 500, 1)
+pyautogui.moveTo(1000, 500, 1)
+pyautogui.moveTo(500, 500, 1)
+pyautogui.moveTo(1000, 500, 1)
+
+pyautogui.alert('continue?')
+pyautogui.moveTo(500, 500, 1)
+pyautogui.moveTo(1000, 500, 1)
+pyautogui.moveTo(500, 500, 1)
+pyautogui.moveTo(1000, 500, 1)
