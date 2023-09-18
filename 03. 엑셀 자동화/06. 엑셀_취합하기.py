3bb7a47b0703a7373fbb1215505f5250223b2fe8
Mochazzzang <kominhyuk0923@gmail.com>
2023-09-19 03:59:59 +0900

    230919_mocha

--- 03. 엑셀 자동화/06. 엑셀_취합하기.py
+++ 03. 엑셀 자동화/06. 엑셀_취합하기.py
@@ -12,4 +12,16 @@ total_ws.title = "data"
 # 헤더 추가
 total_ws.append(['순번', '제품명', '수량', '합계'])
 
- 
+file_list = ['11번가', '스마트스토어', '쿠팡']
+
+for file in file_list:
+    wb = openpyxl.load_workbook(f'03. 엑셀 자동화/{file}.xlsx', data_only=True)
+    ws = wb.active
+    for row in ws.iter_rows(min_row = 2):
+        data = []
+        for cell in row:
+            data.append(cell.value)
+        total_ws.append(data)
+        
+
+total_wb.save('03. 엑셀 자동화/total.xlsx')  
